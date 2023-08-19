c86c0ef097309f9d280b1b22d67c0742d9b3f7de
Hemny Sibrian <hemny.torrealba@naranja.com>
2023-08-19 19:47:46 -0400

    Se agrega un h1.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import { Heading } from "./components/heading/heading";
 import {HelloWorldButton} from "./components/hello-world-button/hello-world-button";
 
+const webpackIsAwesome = new Heading();
+webpackIsAwesome.render();
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
