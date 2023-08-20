fd20602b50cd459486324e9ce4889875f830e84d
Hemny Sibrian <hemny.torrealba@naranja.com>
2023-08-19 20:04:10 -0400

    25 se usa plugin para limpiar la carpeta dist en cada build.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const TerserPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
@@ -59,5 +60,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'styles.[contenthash].css'
     }),
+    new CleanWebpackPlugin()
   ]
 }
